80ade32cc17d202763558748e9e038cf29ccd078
KDKbegin <126341370+KDKbegin@users.noreply.github.com>
2023-03-02 20:40:55 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 #it means from flask module import flask class
 #when we install the frame work diff modules get installed
 #and modules have lowercase names always
-app=Flask(__name__)
+app = Flask(__name__)
+
+
 #to create object of the class Flask
 #name refers from where the it is called
 #execute only print(__name__) o/p=__main__
 @app.route("/")
-def hello():
-  return "This is First Page"
-if(__name__=='__main__'):
-  app.run('0.0.0.0',debug=True)
+def hellowebpage():
+  return render_template('home.html')
 
-  
+
+if (__name__ == '__main__'):
+  app.run('0.0.0.0', debug=True)
