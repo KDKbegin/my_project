6d72d419da73e7b09c5b836dc36884081dd0bf9c
KDKbegin <126341370+KDKbegin@users.noreply.github.com>
2023-03-03 07:52:45 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,4 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+#we are creating a list of dictionary of jobs,where each list item is corresponding
+#to one job.
+JOBS = [{
+  'id': 1,
+  'title': 'Software Engineer',
+  'requirements': 'Frontend + backend',
+  'location': 'Mumbai',
+  'salary': '800000'
+}, {
+  'id': 2,
+  'title': 'Frontend Engineer',
+  'requirements': 'HTML + Javascript + CSS',
+  'location': 'Pune',
+  'salary': '400000'
+}, {
+  'id': 3,
+  'title': 'Backend Engineer',
+  'requirements': 'backend, Python, Databases',
+  'location': 'Banglore',
+  'salary': '850000'
+},
+        {
+  'id': 4,
+  'title': 'Database Administrator',
+  'requirements': 'backend, Python, Databases, MySQL ',
+  'location': 'Pune',
+   
+}
+       
+       ]
 
 #it means from flask module import flask class
 #when we install the frame work diff modules get installed
@@ -11,7 +41,14 @@ app = Flask(__name__)
 #execute only print(__name__) o/p=__main__
 @app.route("/")
 def hellowebpage():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+  #we are passing the list as an argument.providing data to render in the template.
+  #way to insert dynamic data into html/css
+#this will return the data in the json format.
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if (__name__ == '__main__'):
